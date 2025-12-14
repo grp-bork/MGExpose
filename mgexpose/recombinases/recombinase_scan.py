fc724afe92632f1556ff625d752f138d2578ecc8
Christian Schudoma <christian.schudoma@embl.de>
2025-12-14 22:45:17 +0100

    fix recombinase_scan restructure

--- mgexpose/recombinases/recombinase_scan.py
+++ mgexpose/recombinases/recombinase_scan.py
@@ -5,9 +5,9 @@ import pathlib
 
 import pyhmmer
 
-from .gffio import read_prodigal_gff
 from .recombinases import MGE_ALIASES
-from .readers import read_mge_rules
+from ..utils.gffio import read_prodigal_gff
+from ..utils.readers import read_mge_rules
 
 
 RECOMBINASE_SCAN_HEADER = (
