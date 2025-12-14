68e2b811421b024f0e0c2eb3a9109df2977479a2
Christian Schudoma <christian.schudoma@embl.de>
2025-12-14 23:35:47 +0100

    deconvolute cyclic-import issue

--- mgexpose/islands/__init__.py
+++ mgexpose/islands/__init__.py
@@ -1,5 +1,1 @@
 """ Module for genomic island datastructures. """
-
-# from .annotated_genomic_island import AnnotatedGenomicIsland
-# from .genomic_island import GenomicIsland
-# from .mge_genomic_island import MgeGenomicIsland
