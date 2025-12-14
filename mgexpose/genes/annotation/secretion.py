9ba4528682b9ff950f2cefce3252149084e34112
Christian Schudoma <christian.schudoma@embl.de>
2025-12-14 22:09:34 +0100

    fix secretion restructure

--- mgexpose/genes/annotation/secretion.py
+++ mgexpose/genes/annotation/secretion.py
@@ -1,11 +1,12 @@
-from ..readers import parse_macsyfinder_report
+""" Module to add secretion system annotations. """
+from ...utils.readers import parse_macsyfinder_report
 
 
 def add_secretion_systems(genes, fn, rules_fn):
-	""" Add information from txsscan """
-	for gene_id, secretion_data in parse_macsyfinder_report(fn, rules_fn):
-		gene = genes.get(gene_id)
-		if gene is not None:
-			for sgene, system, rule, *_ in secretion_data:
-				gene.secretion_systems.append(f"{sgene}:{system}")
-				gene.secretion_rules.append(rule)
+    """ Add information from txsscan """
+    for gene_id, secretion_data in parse_macsyfinder_report(fn, rules_fn):
+        gene = genes.get(gene_id)
+        if gene is not None:
+            for sgene, system, rule, *_ in secretion_data:
+                gene.secretion_systems.append(f"{sgene}:{system}")
+                gene.secretion_rules.append(rule)
