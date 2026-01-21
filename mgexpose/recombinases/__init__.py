16267dc13ef81cf6b29245eae9069be6afbae646
Christian Schudoma <christian.schudoma@embl.de>
2026-01-21 16:45:07 +0100

    add parse_recombinase_string

--- mgexpose/recombinases/__init__.py
+++ mgexpose/recombinases/__init__.py
@@ -1,3 +1,21 @@
 """ Module for recombinase functions. """
 
+from collections import Counter
+
 from .recombinases import MGE_ALIASES, MgeRule
+
+
+def parse_recombinase_string(recombinase_str):
+	try:
+		recombinases = Counter(
+			dict(
+				(key, int(value))
+				for key, value in (
+					item.split(":") for item in recombinase_str.split(",")
+				)
+			)
+		)
+	except Exception as exc:
+		raise ValueError(f"recombinase string weird? {recombinase_str}") from exc
+	
+	return recombinases
