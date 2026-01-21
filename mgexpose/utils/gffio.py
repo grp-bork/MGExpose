aa71820a7b01d094a1060c45fcacc5f17c191ae1
Christian Schudoma <christian.schudoma@embl.de>
2026-01-21 16:41:31 +0100

    refactor island parsing and add various utility functions to gffio

--- mgexpose/utils/gffio.py
+++ mgexpose/utils/gffio.py
@@ -6,30 +6,45 @@ from ..islands.genomic_island import GenomicIsland
 from ..islands.mge_genomic_island import MgeGenomicIsland
 
 
+def get_source_column(source_db=None,):
+    return ("proMGE", f"proMGE_{source_db}")[bool(source_db)]
+
+
+def get_attrib_str(attribs):
+    return ";".join(f"{item[0]}={item[1]}" for item in attribs.items() if item[1])
+
+
+def parse_gff_attribs(attrib_str):
+    try:
+        attribs = dict(item.split("=") for item in attrib_str.split(";"))
+    except Exception as exc:
+        raise ValueError(f"problem parsing gff attribute string: {attrib_str}") from exc
+    return attribs
+
+
+
 def read_island_gff(fn, island_cls):
     """ Read island gff """
-    with open(fn, "rt", encoding="UTF-8") as _in:
-        island = None
-        for line in _in:
-            line = line.strip()
-            if line and line[0] != "#":
-                cols = line.split("\t")
-                if cols[2] in ("region", "mobile_genetic_element"):
-                    if island is not None:
-                        yield island
-                    # print("NEW ISLAND", cols)
-                    island = island_cls.from_gff(*cols)
-                elif cols[2] == "gene":
-                    gene = Gene.from_gff(*cols)
-                    # print(gene)
-                    if island is not None:
-                        if not island.genes:
-                            island.genes = set()
-                        island.genes.add(gene)
-                    else:
-                        raise ValueError("Found gene but no island.")
-        if island is not None:
-            yield island
+    island = None
+    for line in get_lines_from_chunks(fn):
+        if line and line[0] != "#":
+            cols = line.split("\t")
+            if cols[2] in ("region", "mobile_genetic_element"):
+                if island is not None:
+                    yield island
+                island = island_cls.from_gff(*cols)
+            elif cols[2] == "gene":
+                gene = Gene.from_gff(*cols)
+                if island is not None:
+                    if not island.genes:
+                        island.genes = set()
+                    island.genes.add(gene)
+                    if island.speci is None:
+                        island.speci = gene.speci
+                else:
+                    raise ValueError("Found gene but no island.")
+    if island is not None:
+        yield island
 
 
 def read_genomic_islands_gff(fn):
@@ -50,13 +65,4 @@ def read_prodigal_gff(f):
     for line in get_lines_from_chunks(f):
         if line and line[0] != "#":
             cols = line.rstrip().split("\t")
-            # _id = [
-            #     item.split("=")[1]
-            #     for item in line[8].split(";")
-            #     if item.startswith("ID")
-            # ][0]
-            # gene_id = f"{line[0]}_{_id.split('_')[1]}"
-            # yield gene_id, line
-            # yield _id, line
-
             yield Gene.from_gff(*cols,)
