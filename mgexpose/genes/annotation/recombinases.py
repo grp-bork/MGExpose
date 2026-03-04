acbc859ddc4d4e3368b4473a6e740e29f5b5dc24
Christian Schudoma <christian.schudoma@embl.de>
2026-03-04 14:05:31 +0100

    silence debug print

--- mgexpose/genes/annotation/recombinases.py
+++ mgexpose/genes/annotation/recombinases.py
@@ -4,7 +4,6 @@
 def add_recombinases(recombinases, genes,):
     """ Add information from recombinase scan """
     for gene_id, recombinase in recombinases:
-        print("RECOMBINASE", gene_id, recombinase)
         gene = genes.get(gene_id)
         if gene is not None:
             gene.recombinase = recombinase
