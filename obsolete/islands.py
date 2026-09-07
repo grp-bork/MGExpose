74ff7b34faea9449581da99a9670eea59ab1f78d
Christian Schudoma <christian.schudoma@embl.de>
2026-09-08 00:06:25 +0200

    re-add recognition of txssscan signals

--- obsolete/islands.py
+++ obsolete/islands.py
@@ -296,8 +296,11 @@ class AnnotatedGenomicIsland(GenomicIsland):
                         _, system = system.split(":")
                     except ValueError:
                         continue
-                    if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
-                        has_mandatory_system = True
+                    conjscan_signal = system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",)
+                    txssscan_signal = system.upper()[:4] in ("CONJ", "T4SS",)
+                    has_mandatory_system |= (conjscan_signal or txssscan_signal)
+                    # if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
+                        # has_mandatory_system = True
                     if rule is not None:
                         cm_counts[(system, False)] += 1
                         cm_counts[(system, True)] += 1
