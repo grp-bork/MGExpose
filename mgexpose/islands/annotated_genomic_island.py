170a2260666a10a038d5bdfc944655b7d722f41f
Christian Schudoma <christian.schudoma@embl.de>
2026-09-08 15:55:12 +0200

    re-add recognition of txssscan signals

--- mgexpose/islands/annotated_genomic_island.py
+++ mgexpose/islands/annotated_genomic_island.py
@@ -65,12 +65,15 @@ class AnnotatedGenomicIsland(GenomicIsland):
 
                 has_mandatory_system = False
                 for system, rule in zip(gene.secretion_systems, gene.secretion_rules):
+                    txsscan_signal = False
                     try:
                         _, system = system.split(":")
                     except ValueError:
-                        continue
-                    if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
-                        has_mandatory_system = True
+                        txssscan_signal = system.upper()[:4] in ("CONJ", "T4SS",)                        
+                    conjscan_signal = system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",)
+                    has_mandatory_system |= (conjscan_signal or txssscan_signal)
+                    # if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
+                    #     has_mandatory_system = True
                     if rule is not None:
                         cm_counts[(system, False)] += 1
                         cm_counts[(system, True)] += 1
