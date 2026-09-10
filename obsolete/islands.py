c3fe7382a780dd69e1eaf284700b7f1966fc8779
Christian Schudoma <christian.schudoma@embl.de>
2026-09-10 09:29:43 +0200

    revert islands.py

--- obsolete/islands.py
+++ obsolete/islands.py
@@ -292,15 +292,12 @@ class AnnotatedGenomicIsland(GenomicIsland):
 
                 has_mandatory_system = False
                 for system, rule in zip(gene.secretion_systems, gene.secretion_rules):
-                    txssscan_signal = False
                     try:
                         _, system = system.split(":")
                     except ValueError:
-                        txssscan_signal = system.upper()[:4] in ("CONJ", "T4SS",)                        
-                    conjscan_signal = system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",)
-                    has_mandatory_system |= (conjscan_signal or txssscan_signal)
-                    # if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
-                        # has_mandatory_system = True
+                        continue
+                    if system.split("/")[1].split("_")[0] in ("dCONJ", "T4SS", "MOB",):
+                        has_mandatory_system = True
                     if rule is not None:
                         cm_counts[(system, False)] += 1
                         cm_counts[(system, True)] += 1
