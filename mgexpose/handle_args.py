c4de27d2ad1f5982c7b01b045a411b2aec80b2b6
Christian Schudoma <christian.schudoma@embl.de>
2026-09-11 14:52:54 +0200

    remove redundant --recombinase_hits from reannotate_ap

--- mgexpose/handle_args.py
+++ mgexpose/handle_args.py
@@ -167,7 +167,7 @@ def handle_args(args):
 
     reannotate_ap.set_defaults(func=None)  # TODO
 
-    reannotate_ap.add_argument("--recombinase_hits", type=str)
+    # reannotate_ap.add_argument("--recombinase_hits", type=str)
     reannotate_ap.add_argument("--pyhmmer_input", action="store_true")
     reannotate_ap.add_argument("--phage_eggnog_data", type=str)
     reannotate_ap.add_argument("--phage_filter_terms", type=str)
